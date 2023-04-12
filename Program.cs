2ff321ced2d200ef16a5ecc2a21bc114d9fd5e71
Artem <130403999+Art-ad-hub@users.noreply.github.com>
2023-04-12 21:55:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystals;
+            int pricePurchase = 5;
+
+            Console.WriteLine("Добро пожаловать в магазин КРИСТАЛ! Сегодня кристалы по " + pricePurchase + " золотых.");
+            Console.Write("Сколько у Вас золота: ");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристалов вы хотите купить: ");
+            crystals = Convert.ToInt32(Console.ReadLine());
+            gold -= (crystals * pricePurchase);
+            Console.WriteLine($"Поздравляю, вы купили {crystals} кристалов, а золота теперь у вас {gold}.");
+            Console.ReadKey();
+        }
+    }
+}
